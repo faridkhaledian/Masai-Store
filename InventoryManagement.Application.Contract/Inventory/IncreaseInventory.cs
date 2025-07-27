4a85a656f0a11578f5e23a0c6cd01fb98b21f759
FaridKhaledian <Farid.Khaledian.ai@gmail.com>
2025-07-27 03:45:03 -0700

    Inventory Ui Implemented

--- InventoryManagement.Application.Contract/Inventory/IncreaseInventory.cs
+++ InventoryManagement.Application.Contract/Inventory/IncreaseInventory.cs
@@ -1,9 +1,14 @@
-﻿namespace InventoryManagement.Application.Contract.Inventory
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Application.Contract.Inventory
 {
    public class IncreaseInventory
     {
         public long InventoryId { get; set; }
+        [Required(ErrorMessage =ValidationMessages.IsRequired) ]
         public long Count { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Description { get; set; }
     }
 }
--- InventoryManagement.Application.Contract/Inventory/ReduceInventory.cs
+++ InventoryManagement.Application.Contract/Inventory/ReduceInventory.cs
@@ -1,10 +1,15 @@
-﻿namespace InventoryManagement.Application.Contract.Inventory
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Application.Contract.Inventory
 {
     public class ReduceInventory
     {
         public long InventoryId { get; set; }
         public long ProductId { get; set;}
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public long Count { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Description { get; set; }
         public long  OrderId { get; set; }
     }
